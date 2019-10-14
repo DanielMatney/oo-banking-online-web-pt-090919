9e9f463674a5c80e16b7a2d77a119f10973d9f40
Daniel Matney <danieljmatney@gmail.com>
2019-10-14 01:36:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,8 +19,8 @@ class Transfer
       receiver.balance += @amount
       @status = "complete"
     elsif sender.valid? != true || receiver.valid? != true
-      p "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      p "Transaction rejected. Please check your account balance."
     end
   end
 end
